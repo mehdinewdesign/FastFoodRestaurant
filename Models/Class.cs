d61e22df43d5fa4758aefcf508432cbbdabbd1bb
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-09-24 14:36:42 -0300

    Initial Commit

---
+++ Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodRestaurant.Models
+{
+    class Class
+    {
+        // This class is for tests only
+    }
+}
