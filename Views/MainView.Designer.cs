6436787ab4c0e8eb8d4d3f2870753beb6d03f572
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-09-24 16:34:06 -0300

    Add Home button to left menu
    - Add home button
    - Change default style to flat
    - Add icon home.

--- Views/MainView.Designer.cs
+++ Views/MainView.Designer.cs
@@ -30,11 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
             this.MainPanel = new System.Windows.Forms.Panel();
+            this.HomeButton = new System.Windows.Forms.Button();
             this.TopPanel = new System.Windows.Forms.Panel();
             this.MainLogoPainel = new System.Windows.Forms.Panel();
+            this.SubtitleMainView = new System.Windows.Forms.Label();
+            this.MainTitle = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
             this.MainLogoPainel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -42,33 +44,72 @@
             // MainPanel
             // 
             this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.MainPanel.Controls.Add(this.HomeButton);
             this.MainPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.MainPanel.Location = new System.Drawing.Point(0, 0);
             this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(200, 591);
+            this.MainPanel.Size = new System.Drawing.Size(177, 591);
             this.MainPanel.TabIndex = 0;
             this.MainPanel.TabStop = true;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HomeButton.ForeColor = System.Drawing.Color.White;
+            this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
+            this.HomeButton.Location = new System.Drawing.Point(0, 98);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(177, 54);
+            this.HomeButton.TabIndex = 3;
+            this.HomeButton.Text = "      Home";
+            this.HomeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.HomeButton.UseVisualStyleBackColor = true;
             // 
             // TopPanel
             // 
             this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(41)))), ((int)(((byte)(28)))));
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.TopPanel.Location = new System.Drawing.Point(200, 0);
+            this.TopPanel.Location = new System.Drawing.Point(177, 0);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(867, 10);
+            this.TopPanel.Size = new System.Drawing.Size(890, 10);
             this.TopPanel.TabIndex = 1;
             // 
             // MainLogoPainel
             // 
             this.MainLogoPainel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(41)))), ((int)(((byte)(28)))));
-            this.MainLogoPainel.Controls.Add(this.label2);
-            this.MainLogoPainel.Controls.Add(this.label1);
+            this.MainLogoPainel.Controls.Add(this.SubtitleMainView);
+            this.MainLogoPainel.Controls.Add(this.MainTitle);
             this.MainLogoPainel.Controls.Add(this.pictureBox1);
-            this.MainLogoPainel.Location = new System.Drawing.Point(269, 0);
+            this.MainLogoPainel.Location = new System.Drawing.Point(230, 0);
             this.MainLogoPainel.Name = "MainLogoPainel";
-            this.MainLogoPainel.Size = new System.Drawing.Size(109, 146);
+            this.MainLogoPainel.Size = new System.Drawing.Size(109, 152);
             this.MainLogoPainel.TabIndex = 2;
             this.MainLogoPainel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // SubtitleMainView
+            // 
+            this.SubtitleMainView.AutoSize = true;
+            this.SubtitleMainView.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SubtitleMainView.ForeColor = System.Drawing.Color.White;
+            this.SubtitleMainView.Location = new System.Drawing.Point(14, 117);
+            this.SubtitleMainView.Name = "SubtitleMainView";
+            this.SubtitleMainView.Size = new System.Drawing.Size(82, 17);
+            this.SubtitleMainView.TabIndex = 3;
+            this.SubtitleMainView.Text = "Restaurants";
+            // 
+            // MainTitle
+            // 
+            this.MainTitle.AutoSize = true;
+            this.MainTitle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MainTitle.ForeColor = System.Drawing.Color.White;
+            this.MainTitle.Location = new System.Drawing.Point(13, 98);
+            this.MainTitle.Name = "MainTitle";
+            this.MainTitle.Size = new System.Drawing.Size(83, 19);
+            this.MainTitle.TabIndex = 3;
+            this.MainTitle.Text = "Fast foods";
+            this.MainTitle.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
@@ -79,29 +120,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(13, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 19);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Fast foods";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(14, 117);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Restaurants";
             // 
             // MainView
             // 
@@ -117,6 +135,7 @@
             this.Name = "MainView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainView";
+            this.MainPanel.ResumeLayout(false);
             this.MainLogoPainel.ResumeLayout(false);
             this.MainLogoPainel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -129,9 +148,10 @@
         private System.Windows.Forms.Panel MainPanel;
         private System.Windows.Forms.Panel TopPanel;
         private System.Windows.Forms.Panel MainLogoPainel;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MainTitle;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label SubtitleMainView;
+        private System.Windows.Forms.Button HomeButton;
     }
 }
 
