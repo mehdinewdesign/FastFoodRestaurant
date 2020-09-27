d3289cac771cf9d4170e3ad9937b13cd24e84fbc
Dulcineia Donatto <contato.viniciusdonatto@gmail.com>
2020-09-26 21:13:37 -0300

    Implements SelectItem label for DeliveryControl

--- Views/UserControls/DeliveryControl.Designer.cs
+++ Views/UserControls/DeliveryControl.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.SelectedItemLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -40,19 +42,41 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // SelectedItemLabel
+            // 
+            this.SelectedItemLabel.AutoSize = true;
+            this.SelectedItemLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SelectedItemLabel.Location = new System.Drawing.Point(27, 25);
+            this.SelectedItemLabel.Name = "SelectedItemLabel";
+            this.SelectedItemLabel.Size = new System.Drawing.Size(136, 21);
+            this.SelectedItemLabel.TabIndex = 1;
+            this.SelectedItemLabel.Text = "Item Selecionado: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(699, 23);
+            this.textBox1.TabIndex = 2;
+            // 
             // DeliveryControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SelectedItemLabel);
             this.Controls.Add(this.button1);
             this.Name = "DeliveryControl";
             this.Size = new System.Drawing.Size(890, 433);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label SelectedItemLabel;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
