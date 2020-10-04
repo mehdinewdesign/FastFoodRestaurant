fb01b4e36dc4ba6c56ab7ea07a815d8b397e9e82
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-10-04 17:41:44 -0300

    Add First migration and EntityContext class
    - Add first migrations with empty products class.
    - Add EntityContext class for receive database string and DbSet
      attributes.

---
+++ Models/EntityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodRestaurant.Models
+{
+    public class EntityContext : DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Remove string on upload for security reasons.
+            optionsBuilder.UseSqlServer("");
+        }
+
+    }
+}
