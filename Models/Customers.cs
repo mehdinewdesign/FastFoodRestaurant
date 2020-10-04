512e1eab81c07e64af2896d8fbf6e7db735ab266
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-10-04 18:27:33 -0300

    Add Customers class and migrations
    - Add new customers class
    - Add new migration and update database with custumers class DbSet on
      EntityContext class.

---
+++ Models/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodRestaurant.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Products Points { get; set; }
+    }
+}
--- Models/EntityContext.cs
+++ Models/EntityContext.cs
@@ -8,11 +8,12 @@ namespace FastFoodRestaurant.Models
     public class EntityContext : DbContext
     {
         public DbSet<Products> Products { get; set; }
+        public DbSet<Customers> Customers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Remove string on upload for security reasons.
-            optionsBuilder.UseSqlServer("");
+            optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=FastFood; Integrated Security=SSPI");
         }
 
     }
