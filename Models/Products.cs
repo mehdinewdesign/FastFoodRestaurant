2fb8df78703f120156a43d63abef266994c2a7ba
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-10-04 17:24:17 -0300

    Add Procucts class for database context

---
+++ Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodRestaurant.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public byte Points { get; set; }
+    }
+}
