e4717afd5512a8b7fcc58b72f086214b723cfd6c
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-10-04 22:29:57 -0300

    Add new migration for required filds on table customers
    - Add new Migration and update database
    - New migration add Data Annotation tag [required] for email, username
      and password on database.
    - Add RegisterCustomerControl, a UserControl to store register interface
      for the user.

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -9,9 +9,15 @@ namespace FastFoodRestaurant.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
         public byte Points { get; set; }
+        
     }
 }
--- Views/UserControls/CustomersControl.Designer.cs
+++ Views/UserControls/CustomersControl.Designer.cs
@@ -35,7 +35,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -93,20 +93,23 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Already a customer? Log in";
             // 
-            // label4
+            // button1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(345, 324);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(114, 15);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "No? Register instead";
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(345, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "No? Register instead";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // CustomersControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,6 +132,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
