c7f2aa526fe6c14054f6f7216dd7e6637e24e699
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-01 22:48:09 -0300

    Start implementation of dependency injection to avoid multiples request to database, implementation commented for now, not perfectly functional

--- src/Views/UserControls/HomePromoControl.cs
+++ src/Views/UserControls/HomePromoControl.cs
@@ -11,9 +11,17 @@ namespace FastFoodRestaurant.Views
 {
     public partial class HomePromoControl : UserControl
     {
+        
         #region Object Controllers
         HomePromoControlController homepromoController = new HomePromoControlController();
+        //private readonly HomePromoControlController _controller;
         #endregion
+
+        //public HomePromoControl(HomePromoControlController homePromoControlController)
+        //{
+        //    InitializeComponent();
+        //    _controller = homePromoControlController;
+        //}
 
         public HomePromoControl()
         {
