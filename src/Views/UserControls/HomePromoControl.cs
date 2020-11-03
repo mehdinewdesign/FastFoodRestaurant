4512971d4525537e1b51d711669162a5e4cecf82
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-03 03:51:39 -0300

    Implements concurrency and parallelism with Task-Async-Await
    Change the classe HomePromoControl method ButtonClick to use async
    method that return a task for the controller with data from the view to
    be insert into SQL Server database.

--- src/Views/UserControls/HomePromoControl.cs
+++ src/Views/UserControls/HomePromoControl.cs
@@ -6,31 +6,25 @@ using System.Data;
 using System.Text;
 using System.Windows.Forms;
 using FastFoodRestaurant.Controllers;
+using System.Threading.Tasks;
 
 namespace FastFoodRestaurant.Views
 {
     public partial class HomePromoControl : UserControl
     {
-        
+
         #region Object Controllers
         HomePromoControlController homepromoController = new HomePromoControlController();
-        //private readonly HomePromoControlController _controller;
         #endregion
-
-        //public HomePromoControl(HomePromoControlController homePromoControlController)
-        //{
-        //    InitializeComponent();
-        //    _controller = homePromoControlController;
-        //}
 
         public HomePromoControl()
         {
             InitializeComponent();
         }
 
-        private void HomePromoControlOrderNow_Click(object sender, EventArgs e)
+        private async void HomePromoControlOrderNow_Click(object sender, EventArgs e)
         {
-            homepromoController.DbEntryRecordRegister("Burger", 19, 1);
+            await Task.Run(() => homepromoController.DbEntryRecordRegister("Burger", 19, 1));
         }
     }
 }
